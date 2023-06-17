dd6bc4cf17f1e2682622a7793c56b162a16d8396
Jianqiao Mao <60654068+JianqiaoMao@users.noreply.github.com>
2023-06-17 18:27:23 +0100

    Update setup.py

--- PFFRA/setup.py
+++ PFFRA/setup.py
@@ -4,7 +4,10 @@ setup(
     name='PFFRA',
     version='0.1',
     author='Jianqiao Mao',
+    author_email='jxm1417@student.bham.ac.uk',
+    license='GNU GPL-2.0',
     description='An Interpretable Machine Learning technique to analyse the contribution of features in the frequency domain. This method is inspired by permutation feature importance analysis but aims to quantify and analyse the time-series predictive model's mechanism from a global perspective.',
-    packages=['your_package_directory'],
-    install_requires=['dependency1', 'dependency2'],
+    url='https://github.com/JianqiaoMao/PFFRA',
+    packages=['PFFRA'],
+    install_requires=['numpy', 'matplotlib'],
 )
